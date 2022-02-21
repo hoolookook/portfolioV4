fae17b229010fcfe14298740a707166a58d089ea
hoolookook <hoolookook@gmail.com>
2022-02-21 23:16:28 +0900

    renewal media first

--- public/src/resources/js/lib/main.js
+++ public/src/resources/js/lib/main.js
@@ -1,29 +1,5 @@
 // click & scroll event
 
-var body = document.body,
-  html = document.documentElement,
-  intro = document.getElementById("intro"),
-  footer = document.getElementById("footer"),
-  contents = document.getElementById("contents");
-
-var height = Math.max(
-  body.scrollHeight,
-  body.offsetHeight,
-  html.clientHeight,
-  html.scrollHeight,
-  html.offsetHeight
-);
-
-var introH = Math.max(intro.scrollHeight, intro.offsetHeight),
-  footerH = Math.max(footer.scrollHeight, footer.offsetHeight),
-  contentsH = Math.max(contents.scrollHeight, contents.offsetHeight);
-var introHeight = height - (introH + introH / 1.5),
-  footerHeight = height - (footerH + footerH / 1.5),
-  contentsHeight = height - (contentsH + contentsH / 1.5);
-
-// console.log(footerHeight);
-// console.log(contentsHeight);
-console.log(height);
 //-------------------------------------------------------------------------------------------------
 
 $(function () {
@@ -40,23 +16,52 @@ $(function () {
       $("html,body").animate({ scrollTop: posTop }, speed);
     }
   }
-  $("ul li a").click(function () {
+  $("ul li a, .scrollDown a").click(function () {
     // 네비게이션 클릭시
-    console.log("click!");
+    // console.log("click!");
     var direction = $(this).attr("href"); // direction = 클릭한 요소의 href 속성
     scrolling(direction); // direction 을 인자로 함수 실행
     return false; // 본래 이벤트 방지
   });
-  //-------------------------------------------------------------------------------------------------
 
+  $(".allBtn").click(function () {
+    $(".pc, .responsive").finish().fadeIn(1000);
+  });
+  $(".pcBtn").click(function () {
+    $(".pc").finish().fadeIn(1000);
+    $(".responsive").fadeOut(1000);
+  });
+  $(".responBtn").click(function () {
+    $(".responsive").finish().fadeIn(1000);
+    $(".pc").finish().fadeOut(1000);
+  });
+  $(".moBtn").on("click", function () {
+    $(".mobileNav").slideToggle("slow");
+    $(".ham .line").toggleClass("active");
+  });
   // scroll event
   $(window).scroll(function () {
-    var scrollNum = $(window).scrollTop();
-    var footerNum = scrollNum >= footerHeight ? true : false;
+    // 위치값
+    var winTop = $(window).scrollTop();
+    var $footer = document.querySelector("#footer");
+
+    // 거리계산
+    var footerDis =
+      $footer.getBoundingClientRect().top + window.pageYOffset - 300;
     var $navbar = $(".navBar"),
       $navUI = $(".navBar .navUl");
 
-    console.log("현재 스크롤 값은 " + $(window).scrollTop());
+    // console.log("현재 스크롤 값은 " + $(window).scrollTop());
+    var s = window.matchMedia("screen and (max-width: 481px)");
+
+    if (matchMedia(s.media).matches) {
+    } else {
+      if (winTop >= 50) {
+        down($navbar, $navUI);
+      } else {
+        up($navbar, $navUI);
+      }
+    }
 
     function up(sel1, sel2) {
       sel1.stop().hide();
@@ -66,12 +71,7 @@ $(function () {
       sel1.stop().show();
       sel2.stop().show();
     }
-    if (scrollNum > 100) {
-      down($navbar, $navUI);
-    } else {
-      up($navbar, $navUI);
-    }
-    if (footerNum) {
+    if (winTop > footerDis) {
       if (circleBull) circleChart();
     } else {
       circleBull = true;
@@ -79,7 +79,10 @@ $(function () {
   });
   function clickUpIcon() {
     $("#upicon").click(function () {
-      $("html, body").filter(":not(:animated)").animate({ scrollTop: 0 }, 400);
+      $("html, body")
+        .filter(":not(:animated)")
+        .stop()
+        .animate({ scrollTop: 0 }, 400);
       return false;
     });
   }
@@ -88,10 +91,12 @@ $(function () {
     $("#downicon").click(function () {
       $("html, body")
         .filter(":not(:animated)")
+        .stop()
         .animate({ scrollTop: $(document).height() }, 400);
       return false;
     });
   }
+
   clickDwIcon();
   clickUpIcon();
   //-------------------------------------------------------------------------------------------------
@@ -191,61 +196,69 @@ $(function () {
     }
     playAnim();
   });
+  // hover group
+  // $(function () {
+  //   $("a#clickThis")
+  //     .mouseenter(function () {
+  //       $(this).addClass("underline");
+  //     })
+  //     .mouseleave(function () {
+  //       $(this).removeClass("underline");
+  //     });
+  // });
 });
 
 // work changeText
-// $(function () {
-//   // 미디어쿼리 js 관리
-//   var mql = window.matchMedia(
-//     "screen and (min-width: 468px) and (max-width: 767px)"
-//   );
-
-//   // 복수의 h3 선택
-//   var h3List = document.querySelectorAll("#chanH3");
-//   // h3 태그 인덱스 확인
-//   var h3ListLength = h3List.length;
-
-//   function ChangeName(num, name) {
-//     return (h3List[num].innerHTML = name);
-//   }
-//   for (var i = 0; i < h3ListLength; i++) {
-//     console.log(h3List[i].innerText);
-//     mql.addListener(function (e) {
-//       if (e.matches) {
-//         ChangeName(0, "l");
-//         ChangeName(1, "n");
-//         ChangeName(2, "c");
-//         ChangeName(3, "n");
-//         ChangeName(4, "c");
-//         ChangeName(5, "c");
-//         ChangeName(6, "c");
-//         ChangeName(7, "c");
-//         ChangeName(8, "c");
-//       } else {
-//         ChangeName(0, "lotte");
-//         ChangeName(1, "naver");
-//         ChangeName(2, "coupang");
-//         ChangeName(3, "neungyule");
-//         ChangeName(4, "coming...");
-//         ChangeName(5, "coming...");
-//         ChangeName(6, "coming...");
-//         ChangeName(7, "coming...");
-//         ChangeName(8, "coming...");
-//       }
-//     });
-//   }
-// });
-// hover
 $(function () {
-  $(".openPdf")
-    .mouseover(function () {
-      $(this + "#chanH3").css({
-        display: "none",
-      });
-    })
-    .mouseout(function () {
-      $(this + "#chanH3").css({
-        display: "block",
-      });
+  // 미디어쿼리 js 관리
+  var mql = window.matchMedia("screen and (max-width: 767px)");
+
+  // 복수의 h3 선택
+  var h3List = document.querySelectorAll("#chanH3");
+  // h3 태그 인덱스 확인
+  var h3ListLength = h3List.length;
+
+  function ChangeName(num, name) {
+    return (h3List[num].innerHTML = name);
+  }
+  for (var i = 0; i < h3ListLength; i++) {
+    console.log(h3List[i].innerText);
+    mql.addListener(function (e) {
+      if (e.matches) {
+        ChangeName(0, "l");
+        ChangeName(1, "n");
+        ChangeName(2, "c");
+        ChangeName(3, "n");
+        ChangeName(4, "c");
+        ChangeName(5, "c");
+        ChangeName(6, "c");
+        ChangeName(7, "c");
+        ChangeName(8, "c");
+      } else {
+        ChangeName(0, "lotte");
+        ChangeName(1, "naver");
+        ChangeName(2, "coupang");
+        ChangeName(3, "neungyule");
+        ChangeName(4, "coming...");
+        ChangeName(5, "coming...");
+        ChangeName(6, "coming...");
+        ChangeName(7, "coming...");
+        ChangeName(8, "coming...");
+      }
     });
+  }
 });
+// hover
+// $(function () {
+//   $(".openPdf")
+//     .mouseover(function () {
+//       $(this + "#chanH3").css({
+//         display: "none",
+//       });
+//     })
+//     .mouseout(function () {
+//       $(this + "#chanH3").css({
+//         display: "block",
+//       });
+//     });
+// });
